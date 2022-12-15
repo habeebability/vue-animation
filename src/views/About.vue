4f7bc44e03b018aa657af24fc956a39687c2bc40
Habeebability <awoyemi.habeeb@gmail.com>
2022-12-15 12:02:21 +0100

    added gsap, animate the contact,about and home pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <!-- name="fade" -->
+    <!-- @after-enter="afterEnter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+    -->
+    <transition @after-leave="afterLeave" appear @before-enter="beforeEnter" @enter="enter">
+      <h1 v-if="showTitle">About</h1>
+    </transition>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
@@ -8,17 +15,80 @@
 </template>
 
 <script>
-
+import { ref } from "vue";
+import gsap from "gsap";
 export default {
   setup() {
+    const showTitle = ref(true);
+    const beforeEnter = (el) => {
+      console.log("before enter", el);
+      console.log("befefore enter - set intitial state");
+      el.style.transform = "translateY(-60px)";
+      el.style.opacity = 0;
+    };
+    const enter = (el, done) => {
+      console.log("starting to enter - make transitions");
+      console.log(" enter", el);
 
-  }
-}
+      gsap.to(el, {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "bounce.out",
+        onComplete: done,
+      });
+    };
+
+    const afterEnter = (el) => {
+      // el.style.color = "green";
+      console.log("after enter", el);
+      // setTimeout(() => {
+      //   showTitle.value = false;
+      // }, 2000);
+    };
+
+    // const beforeLeave = (el) => {
+    //   el.style.color = "pink";
+    //   console.log("before leave", el);
+    // };
+
+    // const leave = (el) => {
+    //   console.log(" leave", el);
+    // };
+
+    // const afterLeave = (el) => {
+    //   console.log("after leave", el);
+    // };
+    return {
+      showTitle,
+      beforeEnter,
+      enter,
+      afterEnter,
+      // beforeLeave,
+      // leave,
+      // afterLeave,
+    };
+  },
+};
 </script>
 
 <style>
-  .about {
-    max-width: 600px;
-    margin: 20px auto;
-  }
+.about {
+  max-width: 600px;
+  margin: 20px auto;
+}
+
+/* switch transitions */
+
+/* .fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 3s ease;
+}
+.fade-leave-active {
+  transition: all 3s ease;
+} */
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,119 @@
 <template>
   <div class="home">
-    <Toast v-if="showToast" />
+    <transition name="toast">
+      <Toast v-if="showToast" />
+    </transition>
     <Todos @badValue="triggerToast" />
+
+    <!-- <transition name="fade">
+      <div v-if="showP">hello, ninja</div>
+    </transition>
+
+    <button @click="showP = !showP">toggle</button>-->
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import Toast from '../components/Toast'
-import Todos from '../components/Todos'
+import { ref } from "vue";
+import Toast from "../components/Toast";
+import Todos from "../components/Todos";
 
 export default {
   components: { Toast, Todos },
   setup() {
-    const showToast = ref(false)
+    const showToast = ref(false);
+    // const showP = ref(false);
 
     const triggerToast = () => {
       showToast.value = true;
-      setTimeout(() => showToast.value = false, 3000)
-    }
+      setTimeout(() => (showToast.value = false), 3000);
+    };
 
-    return { showToast, triggerToast }
-  }
-}
+    return {
+      showToast,
+      triggerToast,
+      // showP
+    };
+  },
+};
 </script>
 
 <style>
+/* enter classes */
+/* .toast-enter-from {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.toast-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+} */
+.toast-enter-active {
+  /* transition: all 0.3s ease; */
+  animation: wobble 0.5s ease;
+}
 
+/* leave classes */
+.toast-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+.toast-leave-to {
+  opacity: 1;
+  transform: translateY(-60px);
+}
+.toast-leave-active {
+  transition: all 0.3s ease;
+}
+
+@keyframes wobble {
+  0% {
+    transform: translateY(-60px);
+    opacity: 0;
+  }
+  50% {
+    transform: translateY(0);
+
+    opacity: 1;
+  }
+  60% {
+    transform: translateX(8px);
+  }
+  70% {
+    transform: translateX(-8px);
+  }
+  80% {
+    transform: translateX(4px);
+  }
+  90% {
+    transform: translateX(-4px);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+/* .fade-enter-from {
+  opacity: 0;
+} */
+
+/* can be used or not used */
+/* .fade-enter-to {
+  opacity: 1;
+} */
+/* .fade-enter-active {
+  transition: all 2s ease;
+} */
+
+/* this also can be used or not used */
+
+/* .fade-leave-from {
+  opacity: 1;
+} */
+/* .fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-active {
+  transition: all 2s ease;
+} */
 </style>
